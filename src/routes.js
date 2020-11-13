bd19fcc3ce3a22f5b295148c1a74819c8b13268f
Ritzar <joaovictor-fza@hotmail.com>
2020-11-12 23:25:50 -0300

    Co-authored-by: MatheusFilipe95 <mafieu666@hotmail.com>

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import DrainWatterController from './app/controllers/DrainWaterController';
 import GeolocController from './app/controllers/GeolocController';
 import FollowModeController from './app/controllers/FollowModeController';
 import BluetoothConnectionController from './app/controllers/BluetoothConnectionController';
+import DrainCommand from './app/controllers/DrainCommand';
 
 const { Router } = require('express');
 
@@ -17,6 +18,9 @@ routes.get('/drain-watter', DrainWatterController.drainWatter);
 
 // geoloc
 routes.post('/geoloc', GeolocController.receiveGeoloc);
+
+// drain water command
+routes.post('/drain-water-command', DrainCommand.receiveCommand);
 
 // follow mode
 routes.post('/follow', FollowModeController.handleFollowMode);
